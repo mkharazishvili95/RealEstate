6d6c765e8ae5a5f530915df92c65cbcf8704dbd8
mikheil <mikheil@lemondo.com>
2025-02-19 00:33:14 +0400

    AddedQueriesBaseModel

--- RealEstate.Common/obj/Debug/net6.0/RealEstate.Common.AssemblyInfo.cs
+++ RealEstate.Common/obj/Debug/net6.0/RealEstate.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be9e986c0a4493bb6168127201b56f5f75a64f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719af53b91308b65715b3b6e575b0d1fe3b76415")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ RealEstate.Infrastructure/Queries/QueriesBase.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace RealEstate.Infrastructure.Queries
+{
+    public abstract class QueriesBase
+    {
+        string connectionString { get; set; }
+        public QueriesBase(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task<TResponse?> Get<TResponse>(string commandText, Func<NpgsqlDataReader, TResponse> selector) where TResponse : new()
+        {
+            var response = new TResponse();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (NpgsqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    using var reader = await command.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            response = await reader.Select(selector);
+                        }
+                        return response;
+                    }
+                }
+                await connection.CloseAsync();
+            }
+            return default;
+        }
+
+        public async Task<List<TResponse>> GetMany<TResponse>(string commandText, Func<NpgsqlDataReader, TResponse> selector)
+        {
+            var response = new List<TResponse>();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (NpgsqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    using var reader = await command.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                        while (await reader.ReadAsync())
+                        {
+                            var value = await reader.Select(selector);
+                            response.Add(value);
+                        }
+                }
+                await connection.CloseAsync();
+            }
+            return response;
+        }
+    }
+
+    public static class QuerieHelpler
+    {
+        public static async Task<T> Select<T>(this NpgsqlDataReader reader, Func<NpgsqlDataReader, T> selector)
+        {
+            return selector.Invoke(reader);
+        }
+    }
+}
